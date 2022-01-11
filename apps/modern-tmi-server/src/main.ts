9f27913373f5999ac9fff76f650a33b9a5234a0d
kimhj <kimhj@airi.kr>
2022-01-11 17:36:11 +0900

    feat: swagger 연결

--- apps/modern-tmi-server/src/main.ts
+++ apps/modern-tmi-server/src/main.ts
@@ -3,20 +3,46 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+class Server {
+  private app: INestApplication;
+  private port: string | number;
+
+  async start() {
+    this.app = await NestFactory.create(AppModule);
+    this.port = process.env.PORT || 3333;
+
+    this.app.useGlobalPipes(new ValidationPipe({
+      transform: true
+    }));
+
+    this.app.enableCors();
+    this.setupSwagger();
+
+    await this.app.listen(this.port);
+
+    Logger.log(
+      `🚀 Application is running on: http://localhost:${this.port}`
+    );
+  }
+
+  private setupSwagger() {
+    const options = new DocumentBuilder()
+    .setTitle('Modern TMI API')
+    .setDescription('Modern TMI API 문서')
+    .setVersion('0.0.1')
+    .build();
+
+    const document = SwaggerModule.createDocument(this.app, options);
+    SwaggerModule.setup('swagger', this.app, document);
+  }
+
 }
 
-bootstrap();
+const server = new Server();
+server.start();
