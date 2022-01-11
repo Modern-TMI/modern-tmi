08639ace27692d0a407019b90148ad84d6153e33
kimhj <kimhj@airi.kr>
2022-01-11 17:35:28 +0900

    feat: database config

--- apps/modern-tmi-server/src/app/app.module.ts
+++ apps/modern-tmi-server/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'tmi',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
