38cc793d505594513f33af47d576fa1b43d2c55b
kimhj <kimhj@airi.kr>
2022-01-11 10:12:22 +0900

    feat: nest 추가

---
+++ apps/modern-tmi-server/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'modern-tmi-server',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/modern-tmi-server',
+};
