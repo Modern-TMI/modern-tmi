437dbd0bd3000558af03c01a52b1a9935667a07f
nunch <ktojong@gmail.com>
2022-01-13 17:30:45 +0900

    chore: User Entity Swagger 내용 추가

--- apps/modern-tmi-server/src/app/users/users.entity.ts
+++ apps/modern-tmi-server/src/app/users/users.entity.ts
@@ -1,36 +1,45 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { BaseEntity } from '../types';
 import { ApiProperty } from '@nestjs/swagger';
 import { IUser } from './types';
 
 @Entity()
 export class User implements BaseEntity, IUser {
-
   @PrimaryGeneratedColumn()
-  @ApiProperty({description: 'id'})
+  @ApiProperty({ description: 'id' })
   id: number;
 
-  @Column({nullable: false})
-  @ApiProperty({description: '이메일 주소'})
+  @Column({ nullable: false })
+  @ApiProperty({ description: '이메일 주소' })
   email: string;
 
-  @Column({default: true})
-  @ApiProperty({description: '삭제 여부'})
+  @Column({ default: true })
+  @ApiProperty({ description: '삭제 여부' })
   isActive: boolean;
 
-  @Column({nullable: false})
-  @ApiProperty({description: '닉네임'})
+  @Column({ nullable: false })
+  @ApiProperty({ description: '닉네임' })
   nickname: string;
 
   @Column()
-  @ApiProperty({description: '비밀번호'})
+  @ApiProperty({ description: '비밀번호' })
   password: string;
 
+  @Column({ nullable: true })
+  @ApiProperty({ description: 'RefreshToken' })
+  refreshToken?: string;
+
   @UpdateDateColumn()
-  @ApiProperty({description: '수정 일자 '})
+  @ApiProperty({ description: '수정 일자 ' })
   updatedDate: Date;
 
   @CreateDateColumn()
-  @ApiProperty({description: '생성 일자'})
+  @ApiProperty({ description: '생성 일자' })
   createdDate: Date;
 }
