9d21d229e1a774220ce5549e2439570127f45bdf
nunch <ktojong@gmail.com>
2022-02-05 18:17:50 +0900

    feat: comment Entity 정리 및 Repository 위치 변경

--- apps/modern-tmi-server/src/app/module/comments/comment.module.ts
+++ apps/modern-tmi-server/src/app/module/comments/comment.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommentRepository } from './repositories/comment.repository';
+import { CommentRepository } from './comment.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CommentRepository])],
--- apps/modern-tmi-server/src/app/module/comments/entities/comment.entity.ts
+++ apps/modern-tmi-server/src/app/module/comments/entities/comment.entity.ts
@@ -1,31 +1,52 @@
-import {
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 import { CommonEntity } from '../../common.entity';
 import { Tmi } from '../../tmi/entities/tmi.entity';
 
 @Entity()
 export class Comment extends CommonEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
+  @Column({
+    comment: '댓글 내용',
+    type: 'text',
+  })
   contents: string;
 
-  @ManyToOne((type) => User, (user) => user.comment)
+  @Column({
+    comment: '댓글 삭제 여부',
+    type: 'boolean',
+    default: false,
+  })
+  isDeleted: boolean;
+
+  /**
+   * N : 1 관계 설정
+   * @ManyToOne -> 해당 엔티티(Comment) To 대상 엔티티(User)
+   *               여러 댓글들은 한명의 작성자가 작성
+   */
+  @ManyToOne(() => User, (user) => user.comment)
   user: User;
 
-  @ManyToOne((type) => Tmi, (tmi) => tmi.comment)
+  /**
+   * N : 1 관계 설정
+   * @ManyToOne -> 해당 엔티티(Comment) To 대상 엔티티(Tmi)
+   *               여러 댓글들은 하나의 Tmi에 작성
+   */
+  @ManyToOne(() => Tmi, (tmi) => tmi.comment)
   tmi: Tmi;
 
-  @OneToMany((type) => Comment, (comment) => comment.parent)
+  /**
+   * 1 : N 관계 설정
+   * @OneToMany -> 해당 엔티티(Comment) To 대상 엔티티(Comment)
+   *               하나의 댓글 아래에는 대댓글이 달림
+   */
+  @OneToMany(() => Comment, (comment) => comment.parent)
   reply: Comment[];
 
-  @ManyToOne((type) => Comment, (comment) => comment.reply)
+  /**
+   * N : 1 관계 설정
+   * @ManyToOne -> 해당 엔티티(Comment) To 대상 엔티티(Comment)
+   *               여러 대댓글은 하나의 Parent를 가짐
+   */
+  @ManyToOne(() => Comment, (comment) => comment.reply)
   parent: Comment[];
 }
