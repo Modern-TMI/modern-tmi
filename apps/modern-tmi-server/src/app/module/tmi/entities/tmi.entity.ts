717c1074f162399e5581d4c92a4aff6869559b59
nunch <ktojong@gmail.com>
2022-02-05 18:11:25 +0900

    feat: Tmi Entity 정리

--- apps/modern-tmi-server/src/app/module/tmi/entities/tmi.entity.ts
+++ apps/modern-tmi-server/src/app/module/tmi/entities/tmi.entity.ts
@@ -1,10 +1,10 @@
 import {
   Column,
   Entity,
+  JoinTable,
   ManyToMany,
   ManyToOne,
   OneToMany,
-  PrimaryGeneratedColumn,
 } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../../users/entities/user.entity';
@@ -14,43 +14,79 @@ import { History } from '../../history/entities/history.entity';
 import { Favorite } from '../../favorite/entities/favorite.entity';
 import { Comment } from '../../comments/entities/comment.entity';
 
-@Entity('tmi')
+@Entity()
 export class Tmi extends CommonEntity {
-  @PrimaryGeneratedColumn()
-  @ApiProperty({ description: 'id' })
-  id: number;
-
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 255,
+    comment: 'TMI의 제목. 최대 길이는 255자',
+  })
   @ApiProperty({ description: 'TMI 제목' })
   title: string;
 
-  @Column()
+  @Column({
+    type: 'text',
+    comment: 'TMI의 내용. HTML의 형태의 무언가가 들어갈지도 ..?',
+  })
   @ApiProperty({ description: 'TMI 내용' })
   contents: string;
 
-  @Column({ default: 0 })
+  @Column({
+    type: 'int',
+    unsigned: true,
+    default: 0,
+    comment: 'TMI의 조회수',
+  })
   @ApiProperty({ description: 'View Counts' })
   views: number;
 
-  @Column()
+  @Column({
+    type: 'smallint',
+    unsigned: true,
+    comment: 'TMI의 버전. History의 버전과는 상관없이 무조건 증가만 한다.',
+  })
   @ApiProperty({ description: 'TMI 버전' })
   version: number;
 
-  //#region Relations
-  @OneToMany((type) => History, (history) => history.tmi)
+  /**
+   * 1 : N 관계 설정
+   * @OneToMany -> 해당 엔티티(Tmi) To 대상 엔티티(History)
+   *               하나의 Tmi는 여러개의 History를 가진다.
+   */
+  @OneToMany(() => History, (history) => history.tmi)
   history: History[];
 
-  @OneToMany((type) => Favorite, (favorite) => favorite.tmi)
+  /**
+   * 1 : N 관계 설정
+   * @OneToMany -> 해당 엔티티(Tmi) To 대상 엔티티(Favorite)
+   *               하나의 Tmi는 여러개의 즐겨찾기를 가진다.
+   */
+  @OneToMany(() => Favorite, (favorite) => favorite.tmi)
   favorite: Favorite[];
 
-  @OneToMany((type) => Comment, (comment) => comment.tmi)
+  /**
+   * 1 : N 관계 설정
+   * @OneToMany -> 해당 엔티티(Tmi) To 대상 엔티티(Comment)
+   *               하나의 Tmi는 여러개의 댓글을 가진다.
+   */
+  @OneToMany(() => Comment, (comment) => comment.tmi)
   comment: Comment[];
 
-  @OneToMany((type) => Tag, (tag) => tag.name)
+  /**
+   * N : M 관계 설정
+   * @ManyToMany -> 해당 엔티티(Tmi) To 대상 엔티티(Tag)
+   *                여러 Tmi는 여러개의 태그를 가진다.
+   */
+  @ManyToMany(() => Tag)
+  @JoinTable()
   tags: Tag[];
 
-  // 가장 마지막에 수정한 유저만 저장. 전체 유저 기록은 히스토리에서 확인
-  @ManyToOne((type) => User, (user) => user.tmi)
+  /**
+   * N : 1 관계 설정
+   * @ManyToOne -> 해당 엔티티(Tmi) To 대상 엔티티(User)
+   *               Tmi들은 하나의 작성자(User)를 가짐
+   * @description  가장 마지막에 수정한 User만 저장
+   */
+  @ManyToOne(() => User, (user) => user.tmi)
   user: User;
-  //#endregion
 }
