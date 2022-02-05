410e43b2a2d0f8ff8476a6999511864f1a43f5d6
nunch <ktojong@gmail.com>
2022-02-05 16:11:59 +0900

    chore: prettier 적용

--- apps/modern-tmi-server/src/environments/db.config.ts
+++ apps/modern-tmi-server/src/environments/db.config.ts
@@ -1,21 +1,19 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export type DatabaseStatus = 'local'|'dev'| 'prod';
+export type DatabaseStatus = 'local' | 'dev' | 'prod';
 
 const dbConfig: TypeOrmModuleOptions = {
   type: 'mariadb',
   username: 'root',
   password: '1234',
   database: 'tmi',
-  entities: [],
   synchronize: true,
-  autoLoadEntities: true
+  autoLoadEntities: true,
 };
 
 export const setDbConfig = (status: DatabaseStatus) => {
   return Object.assign(dbConfig, {
-    host: status === 'local' ? 'localhost': 'nunch.dev',
-    port: status === 'local'? 3306 : 33067
-  })
+    host: status === 'local' ? 'localhost' : 'nunch.dev',
+    port: status === 'local' ? 3306 : 33067,
+  });
 };
-
